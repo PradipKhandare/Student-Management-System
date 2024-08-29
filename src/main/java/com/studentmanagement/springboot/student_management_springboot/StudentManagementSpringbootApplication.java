dd679fb65affcfd58e0bcb4cb46d40a6ef7f3770
PradipKhandare <pradip.khandare@neutrinotechlabs.com>
2024-08-29 16:48:08 +0530

    Initial commit

---
+++ src/main/java/com/studentmanagement/springboot/student_management_springboot/StudentManagementSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.studentmanagement.springboot.student_management_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studentmanagement/springboot/student_management_springboot/StudentManagementSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentmanagement.springboot.student_management_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
